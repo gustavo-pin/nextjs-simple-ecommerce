3efab35cb3e6e3327a1403e6a339a944f6f6610d
gustavo-pin <pernin6002hg@gmail.com>
2024-05-22 14:17:22 -0300

    add: navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main>
-      <h1 color="black">Ecommerce</h1>
-    </main>
+    <div>
+      <h1>Ecommerce</h1>
+    </div>
   );
 }

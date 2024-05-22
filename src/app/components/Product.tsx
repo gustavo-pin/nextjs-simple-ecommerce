8ab68e144d3579fc251e59ae8c2e650c2ee09bea
gustavo-pin <pernin6002hg@gmail.com>
2024-05-22 15:28:36 -0300

    add: Product component

---
+++ src/app/components/Product.tsx
@@ -0,0 +1,18 @@
+import { ProductType } from "@/types/ProductType"
+
+interface ProductProps {
+    product: ProductType
+}
+
+export default function Product({ product }: ProductProps) {
+    
+    return (
+        <div className="flex flex-col shadow-lg h-96 bg-slate-800 p-5 rounded-md text-gray-300">
+            <div className="relative max-h-72 flex-1">IMG</div>
+            <div className="flex justify-between font-bold my-3">{product.title}</div>
+            <button className="rounded-md bg-teal-600 text-white px-3.5 py-2.5 text-sm text-center">
+                Add to Cart
+            </button>
+        </div>
+    )
+}

a4de546e655220f116d7038d7b1c884bc6ca9a79
gustavo-pin <pernin6002hg@gmail.com>
2024-05-22 15:50:32 -0300

    add: ProductImage component

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,4 +1,5 @@
 import { ProductType } from "@/types/ProductType"
+import ProductImage from "./ProductImage";
 
 interface ProductProps {
     product: ProductType
@@ -8,7 +9,9 @@ export default function Product({ product }: ProductProps) {
     
     return (
         <div className="flex flex-col shadow-lg h-96 bg-slate-800 p-5 rounded-md text-gray-300">
-            <div className="relative max-h-72 flex-1">IMG</div>
+            <div className="relative max-h-72 flex-1">
+                <ProductImage product={product} fill/>
+            </div>
             <div className="flex justify-between font-bold my-3">{product.title}</div>
             <button className="rounded-md bg-teal-600 text-white px-3.5 py-2.5 text-sm text-center">
                 Add to Cart

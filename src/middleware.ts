2c7bc4e4e0295fe2b623732487959c851e66580d
gustavo-pin <pernin6002hg@gmail.com>
2024-05-22 17:11:41 -0300

    fix: middleware protect router

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,11 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
+
+// const isProtectedRoute = createRouteMatcher([
+//   '/(.*)'
+// ]);
 
 export default clerkMiddleware();
 

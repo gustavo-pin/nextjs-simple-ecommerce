0e92dfd564856c1f778d46ce25c94d9274c97ee8
gustavo-pin <pernin6002hg@gmail.com>
2024-05-23 11:39:09 -0300

    :lipstick: feat: cart icon

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -14,16 +14,36 @@ export default function Navbar() {
             Next Store
           </Link>
 
-          <div className="flex items-center gap-8 ">
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+          <div>
+            <div className="flex items-center gap-8 ">
+              <div className="flex items-center cursor-pointer relative">
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='h-5 w-5'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+                  />
+                </svg>
+                <span className="bg-teal-600 text-sm font-bold rounded-full h-4 w-4 flex items-center justify-center absolute left-3 bottom-3">2</span>
+              </div>
 
-            <SignedOut>
-              <button className="rounded-md border-gray-400 px-3 py-2">
-                <SignInButton mode="modal" />
-              </button>
-           </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+
+              <SignedOut>
+                <button className="rounded-md border-gray-400 px-3 py-2">
+                  <SignInButton mode="modal" />
+                </button>
+            </SignedOut>
+            </div>
           </div>
         </nav>
     )

ac5c1293c5776f998148440e039bbafa9947708b
gustavo-pin <pernin6002hg@gmail.com>
2024-05-23 13:40:15 -0300

    :sparkles: feat: product page

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -2,6 +2,7 @@ import { ProductType } from "@/types/ProductType"
 import ProductImage from "./ProductImage";
 import { formatPrice } from "@/lib/utils";
 import AddCart from "./AddCart";
+import Link from "next/link";
 
 
 interface ProductProps {product: ProductType}
@@ -10,18 +11,20 @@ interface ProductProps {product: ProductType}
 export default function Product({ product }: ProductProps) {
     
     return (
-        <div className="flex flex-col shadow-lg h-96 bg-slate-800 p-5 rounded-md text-gray-300">
-            <div className="relative max-h-72 flex-1">
-                <ProductImage product={product} fill/>
-            </div>
+        <Link href={`/product/${product.id}`}>
+            <div className="flex flex-col shadow-lg h-96 bg-slate-800 p-5 rounded-md text-gray-300">
+                <div className="relative max-h-72 flex-1">
+                    <ProductImage product={product} fill/>
+                </div>
 
-            <div className="flex justify-between font-bold my-3">
-                <p className="w-40 truncate">{product.name}</p>
+                <div className="flex justify-between font-bold my-3">
+                    <p className="w-40 truncate">{product.name}</p>
 
-                <p className="text-md text-teal-300">{formatPrice(product.price)}</p>
+                    <p className="text-md text-teal-300">{formatPrice(product.price)}</p>
+                </div>
+                
+                <AddCart product={product}/>
             </div>
-            
-            <AddCart product={product}/>
-        </div>
-    )
+        </Link>
+    ) 
 }

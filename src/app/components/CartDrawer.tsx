83f5cba1e092a8b1134af564632f8192a5743162
gustavo-pin <pernin6002hg@gmail.com>
2024-05-24 11:28:16 -0300

    :sparkles: feat: CartDrawer Checkout component

--- src/app/components/CartDrawer.tsx
+++ src/app/components/CartDrawer.tsx
@@ -3,6 +3,8 @@
 import { formatPrice } from "@/lib/utils";
 import { useCartStore } from "@/store";
 import Image from "next/image";
+import CheckoutButton from "./CheckoutButton";
+import Checkout from "./Checkout";
 
 export default function CartDrawer() {
     const useStore = useCartStore();
@@ -23,51 +25,52 @@ export default function CartDrawer() {
 
                 <div className="border-t border-gray-300 my-4"></div>
 
-                {useStore.cart.map((product) => (
-                    <div key={product.id} className="flex gap-4 py-4">
-                        <Image 
-                            src={product.image}
-                            alt={product.name}
-                            width={120}
-                            height={120}
-                            className="object-cover w-24"
-                        />
-                        <div>
-                            <h2 className="w-42 truncate">{product.name}</h2>
-                            <h2>Quantidade: {product.quantity}</h2>
-                            <p className="text-teal-600 font-bold text-sm">{formatPrice(product.price)}</p>
+                {
+                    useStore.onCheckOut === "cart" && (
+                        <>
+                            {useStore.cart.map((product) => (
+                                <div key={product.id} className="flex gap-4 py-4">
+                                    <Image 
+                                        src={product.image}
+                                        alt={product.name}
+                                        width={120}
+                                        height={120}
+                                        className="object-cover w-24"
+                                    />
+                                    <div>
+                                        <h2 className="w-42 truncate">{product.name}</h2>
+                                        <h2>Quantidade: {product.quantity}</h2>
+                                        <p className="text-teal-600 font-bold text-sm">{formatPrice(product.price)}</p>
 
-                            <button 
-                                onClick={() => useStore.addProduct(product)}
-                                className="py-1 px-2 border rounded-md mt-2 text-sm mr-1"
-                            >Add
-                            </button>
+                                        <button 
+                                            onClick={() => useStore.addProduct(product)}
+                                            className="py-1 px-2 border rounded-md mt-2 text-sm mr-1"
+                                        >Add
+                                        </button>
 
-                            <button
-                                onClick={() => useStore.removeProduct(product)} 
-                                className="py-1 px-2 border rounded-md mt-2 text-sm"
-                            >Remove
-                            </button>
-                        </div>
-                    </div>
-                ))}
+                                        <button
+                                            onClick={() => useStore.removeProduct(product)} 
+                                            className="py-1 px-2 border rounded-md mt-2 text-sm"
+                                        >Remove
+                                        </button>
+                                    </div>
+                                </div>
+                            ))}
+                        </>
+                    )
+                }
 
                 {
-                    useStore.cart.length > 0 && (
+                    useStore.cart.length > 0 && useStore.onCheckOut === "cart" && (
                         <div className="mt-5">
-                            <p className="text-teal-600 font-bold mb-2">
-                                Total: {formatPrice(totalPrice)}
-                            </p>
-                            <button className="w-full rounded-md bg-teal-600 text-white py-2">
-                                Finish order
-                            </button>
+                            <CheckoutButton />
                         </div>
                     )
                 }
 
                 {
-                    useStore.cart.length === 0 && (
-                        <h1>Your cart is empty. </h1>
+                    useStore.onCheckOut === "checkout" && (
+                        <Checkout />
                     )
                 }
             </div>

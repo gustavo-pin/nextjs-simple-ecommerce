60017a4a255d39b09447c639c619654ce0aa7825
gustavo-pin <pernin6002hg@gmail.com>
2024-05-25 10:37:44 -0300

    :wrench: chore: replace domains to remotePatterns in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["files.stripe.com"]
+        remotePatterns: [
+            {
+                hostname: "files.stripe.com",
+                protocol: "https" 
+            }
+        ]
     }
 };
 
